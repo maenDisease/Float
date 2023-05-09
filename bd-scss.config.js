83b28724b93b0c613f66140883073d21be62d256
maenDisease <ninomae2n@gmail.com>
2023-05-08 20:09:53 -0400

    update meta

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -5,6 +5,8 @@ export default {
 		author: "maenDisease",
 		version: "2.0.21",
 		description: "Compact mobile Discord with customization.",
-		source: "https://github.com/maenDisease/Float"
+		source: "https://github.com/maenDisease/Float",
+		website: "https://maendisease.github.io/",
+		invite: "BShu37e4jg"
 	}
 }
